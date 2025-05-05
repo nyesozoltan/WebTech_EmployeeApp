a5c48ecf9098f420d47e1d069f6123a3f9ea5cde
Nyeso Zoltan <zoltan.nyeso@student.uni-miskolc.hu>
2025-05-05 19:44:37 +0200

    Form upgrade+törlés funkció

--- src/app/pages/employee/employee.component.ts
+++ src/app/pages/employee/employee.component.ts
@@ -11,22 +11,31 @@ import { FormsModule } from '@angular/forms';
   templateUrl: './employee.component.html',
   styleUrl: './employee.component.css'
 })
-export class EmployeeComponent {
+export class EmployeeComponent {  
   employees: Employee[] = [];
   employeeService = inject(EmployeeService);
   router = inject(Router);
+  employeeObj: Employee = new Employee();
 
-employeeObj: Employee = new Employee();
+  public departments: string[] = [
+    'Beszerzés',
+    'HR',
+    'Informatika',
+    'Logisztika',
+    'Marketing',
+    'Pénzügy',
+    'Ügyfélszolgálat'
+  ].sort();
 
   ngOnInit() {
-    const stored = this.employeeService.getAllEmployees();
+    const stored = this.employeeService.loadEmployees();
     if (stored.length === 0) {
       this.employeeService.loadInitialEmployees().subscribe(data => {
         localStorage.setItem('employees', JSON.stringify(data));
         this.employees = data;
       });
     } else {
-      this.employees = this.employeeService.loadEmployees();
+      this.employees = stored;
     }
   }
 
@@ -41,7 +50,32 @@ employeeObj: Employee = new Employee();
     }
   } 
 
-  addEmployee() {
+  onSaveEmployee() {
+    // Mezők kitöltésének ellenőrzése
+  const {
+    employeeName,
+    emailId,
+    department,
+    gender,
+    birthDate
+  } = this.employeeObj;
+
+  if (!employeeName || !emailId || !department || !gender || !birthDate) {
+    alert('Minden mezőt ki kell tölteni!');
+    return;
+  }
+
+  // Ellenőrzés: legalább 16 éves?
+  const birth = new Date(birthDate);
+  const today = new Date();
+  const age = today.getFullYear() - birth.getFullYear();
+  const isOldEnough = (age > 16) || (age === 16 && today >= new Date(birth.setFullYear(birth.getFullYear() + 16)));
+
+  if (!isOldEnough) {
+    alert('A dolgozónak legalább 16 évesnek kell lennie!');
+    return;
+  }
+
     const newEmployee = new Employee();
     newEmployee.employeeId = crypto.randomUUID();
     newEmployee.employeeName = this.employeeObj.employeeName;
@@ -49,7 +83,6 @@ employeeObj: Employee = new Employee();
     newEmployee.department = this.employeeObj.department;
     newEmployee.gender = this.employeeObj.gender;
     newEmployee.birthDate = new Date(this.employeeObj.birthDate);
-  
     this.employeeService.addEmployee(newEmployee);
     this.employees = this.employeeService.getAllEmployees();
     this.employeeObj = new Employee();
